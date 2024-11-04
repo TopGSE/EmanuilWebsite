9939baf6205f262d92e5bfa8f7db411e4cf85ec0
TopGSE <solomon.ekov@gmail.com>
2024-11-04 22:41:54 +0100

    responsive menu added for navigation off the website

--- js/script.js
+++ js/script.js
@@ -15,6 +15,18 @@ learnMoreButton.addEventListener('click', () => {
 	window.location.href = 'about.html';
 });
 
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+	document.body.style.backgroundColor = "white";
+  }
+
 
   
   
