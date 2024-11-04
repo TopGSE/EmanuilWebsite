f93bd88e1b3de09b091f50518dafb9b3c791d586
TopGSE <solomon.ekov@gmail.com>
2024-11-04 22:58:34 +0100

    responsive menu adjustments

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ function openNav() {
 	document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
 	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	document.body.style.opacity = "0.9";	
   }
   
   function closeNav() {
